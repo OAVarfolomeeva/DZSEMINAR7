0ba007d0669bd8ee13f9df06246f721334e637f4
Olga Varfolomeeva <romanova_85@mail.ru>
2023-01-30 00:16:40 +0300

    решение задач 47, 50, 52

---
+++ ZADACHA50/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7(ввод индексов) -> такого числа в массиве нет
+// 1, 1 -> 9
+
+void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+   for(int i = 0; i < m; i++)
+    {
+      for(int j = 0; j < n; j++)
+       {
+         matrix[i,j] = new Random().Next(1, 30);
+         Console.Write(matrix[i,j] + "\t");
+       }
+        Console.WriteLine();
+    }
+   
+Console.WriteLine();
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[m,n];
+FillArrayWithRandom(matrix, m, n);
+
+Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int number = 0;
+  for(int i = 0; i <= m; i++)
+    {
+      for(int j = 0; j <= n; j++)
+       {
+         if(a == i & b == j)
+          {
+            number = matrix[i,j];
+          }
+       }
+    }
+
+if(number > 0)
+{
+  Console.WriteLine("Число: " + number);
+}
+else
+{
+   Console.WriteLine("Такого числа нет");
+}
---
+++ ZADACHA52/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+   for(int i = 0; i < m; i++)
+    {
+      for(int j = 0; j < n; j++)
+       {
+         matrix[i,j] = new Random().Next(1, 30);
+         Console.Write(matrix[i,j] + "\t");
+       }
+        Console.WriteLine();
+    }
+   
+Console.WriteLine();
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = new int[m,n];
+FillArrayWithRandom(matrix, m, n);
+
+void NewMatrix(int[,] array, int rows, int columns)
+{
+      for(int j = 0; j < columns; j++)
+       {
+        double pr = 0;
+        for(int i = 0; i < rows; i++)
+       {
+         pr += array[i,j];
+       }
+    Console.WriteLine($"Среднее арифметическое значаний столбца {j} = {Math.Round(pr / rows, 2)}");
+    }
+}
+NewMatrix(matrix, m, n);
